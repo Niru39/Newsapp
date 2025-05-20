914c650a8336abbeeb067ad5c5504c56398ea4b8
Niru maharjan <nirumaharjan55@gmail.com>
2025-05-20 16:27:50 +0545

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,90 +1,13 @@
-//import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import News from './components/News';
 import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import LoadingBar from "react-top-loading-bar";
 import React, { useState } from 'react';
 
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       mode: 'light',
-//       progress:0
-//     };
-//   }
 
-//   pageSize = 9;
-//   apiKey = import.meta.env.VITE_NEWS_API_KEY;
-
-
-//   setProgress =(progress)=>{
-//     this.setState({progress:progress})
-//   }
-
-//   toggleMode = () => {
-//     if (this.state.mode === 'dark') {
-//       this.setState({ mode: 'light' });
-//       document.body.classList.remove('dark-mode');
-//     } else {
-//       this.setState({ mode: 'dark' });
-//       document.body.classList.add('dark-mode');
-//     }
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <Navbar toggleMode={this.toggleMode} mode={this.state.mode} />
-//         <LoadingBar
-//         color="#f11946"
-//         progress={this.state.progress}
-//         />
-//         <Routes>
-//           <Route exact
-//             path="/home"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="general" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="" />}
-//           />
-//           <Route exact
-//             path="/about"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="about" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="" />}
-//           />
-//           <Route exact
-//             path="/business"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="business" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="business" />}
-//           />
-//           <Route exact
-//             path="/general"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="general" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="general" />}
-//           />
-//           <Route exact
-//             path="/entertainment"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="entertainment" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="entertainment" />}
-//           />
-//           <Route exact
-//             path="/health"
-//             element={<News setProgress={this.setProgress} apikey={this.apikey} key="health" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="health" />}
-//           />
-//           <Route exact
-//             path="/science"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="science" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="science" />}
-//           />
-//           <Route exact
-//             path="/sports"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="sports" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="sports" />}
-//           />
-//           <Route exact
-//             path="/technology"
-//             element={<News setProgress={this.setProgress} apikey= {this.apikey} key="technology" mode={this.state.mode} pageSize = {this.pageSize} country="us" category="technology" />}
-//           />
-//         </Routes>
-//         <Footer/>
-//       </div>
-//     );
-//   }
-// }
 
 
 
